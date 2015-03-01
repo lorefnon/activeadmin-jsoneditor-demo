aa05f1da402efe393533cd7178ae23dd1d6e0d00
Lorefnon <lorefnon@gmail.com>
2015-03-01 23:58:56 +0530

    Specify schema

--- db/migrate/20150301182800_create_products.rb
+++ db/migrate/20150301182800_create_products.rb
@@ -1,7 +1,9 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-
+      t.string :name
+      t.text :description
+      t.json :metadata
       t.timestamps
     end
   end
