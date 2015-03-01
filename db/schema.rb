8a778dfe3686d7b64f04d50d19ec4fbd025154f8
Lorefnon <lorefnon@gmail.com>
2015-03-01 23:59:06 +0530

    rake db:migrate
    == 20150301182800 CreateProducts: migrating ===================================
    -- create_table(:products)
       -> 0.0057s
    == 20150301182800 CreateProducts: migrated (0.0058s) ==========================

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150301182332) do
+ActiveRecord::Schema.define(version: 20150301182800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,5 +30,13 @@ ActiveRecord::Schema.define(version: 20150301182332) do
   add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
   add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
   add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
+
+  create_table "products", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.json     "metadata"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
